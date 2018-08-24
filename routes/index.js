06331d75824129bd05b6be8253630eaab958ca32
Lukas Gisder-Dubé <gisderdube@gmail.com>
2018-08-24 12:20:59 +0200

    added book creation

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ const router = express.Router()
 const Book = require('../models/Book')
 
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
     res.render('index')
 })
 
-router.get('/book-list', (req, res, next) => {
+router.get('/book-list', (req, res) => {
     Book.find({})
         .then(books => {
             res.render('book-list', { books })
@@ -15,12 +15,33 @@ router.get('/book-list', (req, res, next) => {
         .catch(console.error)
 })
 
-router.get('/book/:id', (req, res, next) => {
+router.get('/book/:id', (req, res) => {
     Book.findById(req.params.id)
         .then(book => {
             res.render('book-details', { book })
         })
         .catch(console.error)
+})
+
+router.get('/create-book', (req, res) => {
+    res.render('create-book')
+})
+
+router.post('/create-book', (req, res) => {
+    const {
+        title, author, rating, description,
+    } = req.body
+
+    new Book({
+        title,
+        author,
+        rating,
+        description,
+    })
+        .save()
+        .then(book => {
+            res.render('book-details', { book, new: true })
+        })
 })
 
 module.exports = router
